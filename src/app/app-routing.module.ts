8ff10a1a088877ff42b0918a63ad000f904dfd08
Mick Aubin <mick.aubin@gmail.com>
2019-07-29 20:31:56 +0200

    first commit "Examen Angular du 29/07/2019"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {VoituresComponent} from './voitures/voitures.component';
+import {AddVoitureComponent} from './add-voiture/add-voiture.component';
+import {EditVoitureComponent} from './edit-voiture/edit-voiture.component';
+import {MotosComponent} from './motos/motos.component';
+import {AddMotoComponent} from './add-moto/add-moto.component';
+import {EditMotoComponent} from './edit-moto/edit-moto.component';
+import {MarqueVoitureComponent} from './marque-voiture/marque-voiture.component';
+import {AddMarqueVoitureComponent} from './add-marque-voiture/add-marque-voiture.component';
+import {MarqueMotoComponent} from './marque-moto/marque-moto.component';
+import {AddMarqueMotoComponent} from './add-marque-moto/add-marque-moto.component';
+import {DetailVoitureComponent} from './detail-voiture/detail-voiture.component';
+import {DetailMotoComponent} from './detail-moto/detail-moto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'voitures',      component: VoituresComponent},
+  { path: 'add-voiture',      component: AddVoitureComponent},
+  { path: 'detail-voiture/:id',      component: DetailVoitureComponent},
+  { path: 'edit-voiture/:id',      component: EditVoitureComponent},
+  { path: 'motos',      component: MotosComponent},
+  { path: 'add-moto',      component: AddMotoComponent},
+  { path: 'detail-moto/:id',      component: DetailMotoComponent},
+  { path: 'edit-moto/:id',      component: EditMotoComponent},
+  { path: 'marque-voiture',      component: MarqueVoitureComponent},
+  { path: 'add-marque-voiture',      component: AddMarqueVoitureComponent},
+  { path: 'marque-moto',      component: MarqueMotoComponent},
+  { path: 'add-marque-moto',      component: AddMarqueMotoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,48 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './menu/menu.component';
+import { AddMotoComponent } from './add-moto/add-moto.component';
+import { AddVoitureComponent } from './add-voiture/add-voiture.component';
+import { AddMarqueMotoComponent } from './add-marque-moto/add-marque-moto.component';
+import { AddMarqueVoitureComponent } from './add-marque-voiture/add-marque-voiture.component';
+import { VoituresComponent } from './voitures/voitures.component';
+import { MotosComponent } from './motos/motos.component';
+import { MarqueVoitureComponent } from './marque-voiture/marque-voiture.component';
+import { MarqueMotoComponent } from './marque-moto/marque-moto.component';
+import { EditVoitureComponent } from './edit-voiture/edit-voiture.component';
+import { EditMotoComponent } from './edit-moto/edit-moto.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { DetailMotoComponent } from './detail-moto/detail-moto.component';
+import { DetailVoitureComponent } from './detail-voiture/detail-voiture.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AddMotoComponent,
+    AddVoitureComponent,
+    AddMarqueMotoComponent,
+    AddMarqueVoitureComponent,
+    VoituresComponent,
+    MotosComponent,
+    MarqueVoitureComponent,
+    MarqueMotoComponent,
+    EditVoitureComponent,
+    EditMotoComponent,
+    DetailMotoComponent,
+    DetailVoitureComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ModalModule.forRoot(),
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
